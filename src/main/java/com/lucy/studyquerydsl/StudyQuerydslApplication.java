475b2d1b5a006bb31d745280be5dc4f8bbe13436
lucy <loveyou_06@naver.com>
2022-08-30 02:42:39 +0900

    init

---
+++ src/main/java/com/lucy/studyquerydsl/StudyQuerydslApplication.java
@@ -0,0 +1,13 @@
+package com.lucy.studyquerydsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyQuerydslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyQuerydslApplication.class, args);
+	}
+
+}
