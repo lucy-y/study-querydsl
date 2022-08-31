f44bd59bdabed47541713a5e84a1f48ce1c10e02
lucy <lucy@npixel.co.kr>
2022-08-31 16:08:55 +0900

    init querydsl

--- src/main/java/com/lucy/studyquerydsl/controller/MainController.java
+++ src/main/java/com/lucy/studyquerydsl/controller/MainController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MainController {
 
-    @GetMapping("/health")
-    public String health(){
-        return "OK";
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
     }
 
 }
--- src/test/java/com/lucy/studyquerydsl/StudyQuerydslApplicationTests.java
+++ src/test/java/com/lucy/studyquerydsl/StudyQuerydslApplicationTests.java
@@ -1,13 +1,39 @@
 package com.lucy.studyquerydsl;
 
+import com.lucy.studyquerydsl.entity.Hello;
+import com.lucy.studyquerydsl.entity.QHello;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 
 @SpringBootTest
+@Transactional
 class StudyQuerydslApplicationTests {
 
+	@PersistenceContext
+	EntityManager entityManager;
+	
 	@Test
 	void contextLoads() {
+		Hello hello = new Hello();
+		entityManager.persist(hello);
+
+		JPAQueryFactory jpaQueryFactory = new JPAQueryFactory(entityManager);
+		QHello qHello = QHello.hello;
+
+		Hello result = jpaQueryFactory
+				.selectFrom(qHello)
+				.fetchOne();
+
+		Assertions.assertThat(result).isEqualTo(hello);
+		Assertions.assertThat(result.getId()).isEqualTo(hello.getId());
 	}
 
 }
